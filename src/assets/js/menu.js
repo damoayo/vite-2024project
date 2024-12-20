2c8cb1d77ddcab8fc041fdff8bf97b85d3c95763
MJ JI <mamapai@naver.com>
2024-12-21 02:07:33 +0900

    🤩헤더완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,5 +1,16 @@
 const menu = () => {
-  console.log(menu);
+  const headerToggle = document.getElementById("headerToggle");
+  const headerNav = document.querySelector(".header__nav");
+
+  if (headerToggle) {
+    headerToggle.addEventListener("click", () => {
+      headerNav.classList.toggle("show");
+
+      headerToggle.getAttribute("aria-expanded") === "true"
+        ? headerToggle.setAttribute("aria-expanded", "false")
+        : headerToggle.setAttribute("aria-expanded", "true");
+    });
+  }
 };
 
 export default menu;
