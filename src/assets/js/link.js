bd89c0aed98c4ccf405921127a649946e8986f94
MJ JI <mamapai@naver.com>
2024-12-22 07:36:17 +0900

    🤩포트폴리오페이지 완성

--- src/assets/js/link.js
+++ src/assets/js/link.js
@@ -1,5 +1,14 @@
-const link = () => {
-  console.log(link);
-};
+export default function link() {
+  document.querySelectorAll("a[href^='#']").forEach((anchor) => {
+      anchor.addEventListener("click", function(e){
+          e.preventDefault();
 
-export default link;
+          const targetId = this.getAttribute("href");
+          const targetElement = document.querySelector(targetId);
+
+          if(targetElement){
+              targetElement.scrollIntoView({ behavior: "smooth"});
+          }
+      });
+  });
+}
--- src/assets/js/smooth.js
+++ src/assets/js/smooth.js
@@ -1,5 +1,19 @@
-const smooth = () => {
-  console.log(smooth);
-};
+import Lenis from "@studio-freight/lenis";
 
-export default smooth;
+export default function smooth() {
+  const lenis = new Lenis({
+    duration: 1,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
+}
