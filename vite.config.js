d7cf572afb4bafd2af70f3efc6ecb0bc4c38387e
MJ JI <mamapai@naver.com>
2024-12-22 14:59:05 +0900

    확인커밋

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: "src",
   build: {
-    outDir: "../public",
+    outDir: 'dist', // 빌드 결과물이 저장될 디렉터리 이름
+    emptyOutDir: true, // 기존 디렉터리를 비우고 새로 빌드
   },
 });
